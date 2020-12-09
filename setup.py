754c3d058dc6d2104e185f85e658ce004f85da79
taylor-m <53980659+taylor-m@users.noreply.github.com>
2020-12-09 15:05:22 -0600

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from setuptools import setup
 
-setup(name='ds_functions',
+setup(name='ds_functions_pkg',
       version='0.0.1',
       description='useful data science functions',
       author='Taylor Martin',
       author_email='taymart@gmail.com',
-      url='https://github.com/taylor-m/ds_functions',
-      packages=['ds_functions'],
+      url='https://github.com/taylor-m/ds_functions_pkg',
+      packages=['ds_functions_pkg'],
       license='MIT',
       )
