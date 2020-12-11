ce59c26fb8aea8bb9c376861ae635115765d58bf
taylor-m <53980659+taylor-m@users.noreply.github.com>
2020-12-11 15:34:23 -0600

    UPDATED init file with new functions

--- functions_pkg/__init__.py
+++ functions_pkg/__init__.py
@@ -1,2 +1,2 @@
 from .stats_functions import get_95_ci, cles_ind, calc_non_param_ci
-from .supervised_regression import plot_sced_test, plot_top_corrs, top_corrs, tukey_outliers, print_vif, predictions_df
+from .supervised_regression import plot_sced_test, plot_top_corrs, top_corrs, tukey_outliers, print_vif, predictions_df, model_cv_df, model_cv_stats
